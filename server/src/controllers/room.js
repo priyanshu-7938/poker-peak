a0cd94a46c180b2963a43be8caa25f572c4b078c
prateek0093 <pk.prateekkumar93@gmail.com>
2024-02-09 04:35:16 +0530

    The commit late

--- server/src/controllers/room.js
+++ server/src/controllers/room.js
@@ -1,36 +1,170 @@
+import Room from "../models/room.js";
+import User from "../models/user.js";
+import { sendResponse, sendError } from "../middleware/sendResponce.js";
 
-import Room from "../models/room.js"
-import User from "../models/user.js"
-import { sendResponse,sendError } from "../middleware/sendResponce.js";
-
-const creatingARoom = async (req,res)=>{
+const creatingARoom = async (req, res, next) => {
+  try {
     const room = new Room(req.body);
-    await room.save({validataBeforeSave:false});
+    await room.save({ validataBeforeSave: false });
     console.log(room);
     res.send("success");
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-const RegisterForTheRoom = async (req,res)=>{
-    const roomAddress = req.body.id;
-    const _id = req.body?.userId;
-    const token = req.body?.token;
-    const txn = req.body?.txn;
+const RegisterForTheRoom = async (req, res) => {
+  const roomAddress = req.body.id;
+  const _id = req.body?.userId;
+  const token = req.body?.token;
+  const txn = req.body?.txn;
 
-    const bol = User.validateToken( token, _id);
+  const bol = User.validateToken(token, _id);
 
-    if(!bol){
-        res.json({"status":"unauthorised"});
-    }
-    // console.log(req.body.id);
-    const room = await Room.findByAddressValue(roomAddress);
-    if(!room){
-        //meaning there is an error...
-        req.json({"status":"the room not found..."});
-    }
-    res.send("helo"); 
-}
+  if (!bol) {
+    res.json({ status: "unauthorised" });
+  }
+  // console.log(req.body.id);
+  const room = await Room.findByAddressValue(roomAddress);
+  if (!room) {
+    //meaning there is an error...
+    req.json({ status: "the room not found..." });
+  }
+  res.send("helo");
+};
+const roomJoin = async (req, res) => {
+  const contract = req.body.address;
+  const userAddresss = req.body.userAddress;
+  const room = await Room.findByAddressValue(contract);
+  if (room.status !== "resting") {
+    res.json({ status: 100, msg: "game has allready started..." });
+  }
+  const user = await User.findByAddressValue(userAddresss);
 
+  if (user === null || user === undefined) {
+    return res.status(404).json({
+      status: 100,
+      msg: "please join the room with registered wallet adddress.",
+    });
+  }
 
-export { creatingARoom,
-    RegisterForTheRoom
+  const retur = await room.addUser(user._id);
+  // console.log(retur);
+  switch (retur) {
+    case "isin":
+      res.json({ status: 201, msg: "allready existing account" });
+      return;
+    case "full":
+      res.json({ status: 100, msg: "roomm is full." });
+      return;
+  }
+  if (room.users.length === 6 && room.randomNumberGenerated !== undefined) {
+    //gameInit util function....
+    GameInitBaby(contract);
+  }
+  io.emit("update", {});
+  res.json({ ...retur, status: 200 });
+  // gameInit thing.....
+};
+const allRooms = async (req, res) => {
+  const value = await Room.getAllRooms();
+  res.json({ ...value });
+};
+const roomLeave = async (req, res) => {
+  const contract = req.body.address;
+  const userAddresss = req.body.userAddress;
+  const room = await Room.findByAddressValue(contract);
+  //checkingthe satate of the room..
+  if (room.status === "resting") {
+    //meaning the user will simply laeave...
+    const user = await User.findByAddressValue(userAddresss);
+    const retur = await room.removeUser(user._id);
+    res.json({ ...retur, status: 200, msg: "the user left the room." });
+    io.emit("update", {});
+    return;
+  }
+  //meanignthe useer will fold...
+  const user = await User.findByAddressValue(userAddresss);
+  await room.foldUserByAddress(user.address);
+  //calling the contract here.......
+  try {
+    //trying to get contract...
+    const TheContract = await sdk.getContract(contract);
+    const data = await TheContract.call("foldUser", [
+      "user left the Room manually",
+      userAddresss,
+    ]);
+    console.log(data);
+  } catch (error) {
+    res.json({ status: 100, msg: error });
+  }
+  io.emit("update", {});
+  res.json({ status: 200, msg: "you folded" });
+};
+const fetchUsers = async (req, res) => {
+  const contract = req.body.address;
+  const room = await Room.findByAddressValue(contract);
+  const retur = await room.allUsers();
+  res.json(retur);
+};
+const fetchCards = async (req, res) => {
+  console.log("!!fetchedCards");
+
+  const contractAddress = req.body.address;
+  const userAddress = req.body.userAddress;
+  const room = await Room.findByAddressValue(contractAddress);
+  const user = await User.findByAddressValue(userAddress);
+  const cards = await room.getUserCardsVisId(user._id);
+  if (!cards) {
+    console.log("!Card fetch failed.");
+    res.json({ status: 100, msg: "You not part of game bro!" });
+  }
+  console.log(cards);
+  res.json({ status: 200, cards: cards });
+};
+const fetchTabelCards = async (req, res) => {
+  const contractAddress = req.body.address;
+  const room = await Room.findByAddressValue(contractAddress);
+  switch (room.status) {
+    // enum: ['resting','firstloop','secondloop','thirdloop','ended'],
+    case "0":
+      //meaning the cards not been displayed..
+      res.json({ status: 100, msg: "game not started yet" });
+      return;
+    case "1":
+      const result = await room.getFirst3Cards();
+      res.json({ status: 200, cards: result });
+      return;
+    case "2":
+      const result1 = await room.getFirst4Cards();
+      res.json({ status: 200, cards: result1 });
+      return;
+    case "3":
+    case "4":
+      const result2 = await room.getFirst5Cards();
+      res.json({ status: 200, cards: result2 });
+      return;
+  }
+  const cards = await room.getUserCardsVisId(user._id);
+  if (!cards) {
+    res.json({ status: 100, msg: "You not part of game bro!" });
+    return;
+  }
+  res.json({ status: 200, cards: cards });
+};
+const roomData = async (req, res) => {
+  const contractAddress = req.body.address;
+  const room = await Room.findByAddressValue(contractAddress);
+  const dataToSend = await room.getSanitizedRoomInfo();
+};
+export {
+  creatingARoom,
+  RegisterForTheRoom,
+  roomJoin,
+  allRooms,
+  roomLeave,
+  fetchUsers,
+  fetchCards,
+  fetchTabelCards,
+  roomData,
 };
--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config({});
 const connectDB = async () => {
-    console.log("evn ; ", process.env.MONGO_END_POINT);
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGO_END_POINT,{dbName: 'liteflow'});
-        console.log(`\nMONGODB connected !! DB NAME: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB error occured: ", error);
-        process.exit(1);
-    }
-}
+  //   console.log("evn ; ", process.env);
+  try {
+    const connectionInstance = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/",
+      { dbName: "liteflow" }
+    );
+    console.log(
+      `\nMONGODB connected !! DB NAME: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB error occured: ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
